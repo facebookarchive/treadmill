5decca6a327a0b754b5a966b2063a3e1e05ecd29
Yunqi Zhang <yunqifb@fb.com>
2015-03-27 10:54:47 -0700

    Initial checkin for autoconf
    Summary: Initial checkin for the configuration files to build treadmill outside
    facebook.
    Not ready for review yet, but I'd love to get suggestions from you.
    
    Reviewed By: @meyering
    
    Differential Revision: D1370985

---
+++ scripts/install_ubuntu_14.04.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+[ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
+[ -n "$2" ] || ( echo "Service type missing"; exit 1 )
+
+sudo apt-get update
+
+sudo apt-get install -y \
+  gcc-4.8 g++-4.8 libboost1.54-dev libboost-thread1.54-dev \
+  libboost-filesystem1.54-dev libboost-system1.54-dev libboost-regex1.54-dev \
+  libboost-python1.54-dev libboost-context1.54-dev ragel autoconf unzip \
+  git libtool python-dev cmake libssl-dev libcap-dev libevent-dev \
+  libgtest-dev libsnappy-dev scons binutils-dev make \
+  wget libdouble-conversion-dev libgflags-dev libgoogle-glog-dev
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
+cd $(dirname $0)
+
+./get_and_build_everything.sh ubuntu-14.04 $1 $2 $3
---
+++ scripts/recipes/mcrouter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+[ -d mcrouter ] || git clone https://github.com/facebook/mcrouter
+
+cd "$PKG_DIR/mcrouter/mcrouter/"
+autoreconf --install
+LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH" \
+  LD_RUN_PATH="$PKG_DIR/folly/folly/test/.libs:$INSTALL_DIR/lib" \
+  LDFLAGS="-L$PKG_DIR/folly/folly/test/.libs -L$INSTALL_DIR/lib" \
+  CPPFLAGS="-I$PKG_DIR/folly/folly/test/gtest-1.6.0/include -I$INSTALL_DIR/include -I$PKG_DIR/folly -I$PKG_DIR/double-conversion" \
+  ./configure --prefix="$INSTALL_DIR"
+make $MAKE_ARGS && make install $MAKE_ARGS
