65ad0a9e7a57471be5a58dcd8c834b92b55eb9d0
Bin Zhang <bin23@fb.com>
2021-11-16 20:21:00 -0800

    Attach unique replay id to request.
    Summary: Attach unique replay id to request. Implement feature request - https://fb.workplace.com/groups/perflab.radar/permalink/7172990472749506/
    
    Differential Revision: D32397273
    
    fbshipit-source-id: 2e051ad40cbca0ffa57123829ef8b973b79eb392

---
+++ TreadmillConst.h
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include <string_view>
+
+namespace facebook {
+namespace windtunnel {
+namespace treadmill {
+
+constexpr std::string_view kUniqueReplayID = "unique_replay_id";
+
+} // namespace treadmill
+} // namespace windtunnel
+} // namespace facebook
